2dea4c5505716b635cdd1358019bd41c150d38a5
Nikhil <yde773786@gmail.com>
2024-05-16 16:02:08 -0400

    added kernel configs implementation

--- net/adoptopenjdk/bumblebench/jitserver/JITserver.java
+++ net/adoptopenjdk/bumblebench/jitserver/JITserver.java
@@ -4,6 +4,7 @@ import net.adoptopenjdk.bumblebench.core.MicroBench;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,13 +12,17 @@ public final class JITserver extends MicroBench {
 
     // Classes and corresponding invocation counts
     static final HashMap<Class<? extends MicroBench>, Integer> classesToInvocation;
+    static final boolean isMultiThreaded;
 
     static {
         classesToInvocation = option("classesToInvoc", new HashMap<>());
+        isMultiThreaded = option("multi-threaded", true);
     }
 
     @Override
     protected long doBatch(long numIterations) throws InterruptedException {
+
+        ArrayList<Thread> threads = new ArrayList<>();
 
         for (long i = 0; i < numIterations; i++) {
 
@@ -28,21 +33,49 @@ public final class JITserver extends MicroBench {
                 Integer invocationCountValue = classIntegerEntry.getValue();
 
                 for(int j = 0; j < invocationCountValue; j++){
+                    Method methodReq;
                     try {
-                        Method methodReq = classKey.getDeclaredMethod("doBatch", long.class);
-                        methodReq.setAccessible(true);
-                        methodReq.invoke(classKey.newInstance(), invocationCountValue);
+                        methodReq = classKey.getDeclaredMethod("doBatch", long.class);
                     } catch (NoSuchMethodException e) {
-                        System.err.println("Class needs to extend doBatch!");
+                        System.err.println("doBatch not implemented");
                         throw new RuntimeException(e);
-                    } catch (InvocationTargetException | IllegalAccessException | InstantiationException e) {
-                        System.err.println("Could not dynamically initiate doBatch");
-                        throw new RuntimeException(e);
+                    }
+                    methodReq.setAccessible(true);
+
+                    if(isMultiThreaded){
+                        // Thread responsible for spawning a doBatch
+                        Thread t = getThread(methodReq, classKey);
+                        threads.add(t);
+                    }
+                    else {
+                        try {
+                            methodReq.invoke(classKey.newInstance(), 1);
+                        } catch (InvocationTargetException | IllegalAccessException | InstantiationException e) {
+                            System.err.println("Could not dynamically initiate doBatch");
+                            throw new RuntimeException(e);
+                        }
                     }
                 }
             }
         }
 
+        for(Thread thread : threads){
+            thread.join();
+        }
+
         return numIterations;
+    }
+
+    private static Thread getThread(Method methodReq, Class<? extends MicroBench> classKey) {
+        Thread t = new Thread(() -> {
+            try {
+                methodReq.invoke(classKey.newInstance(), 1);
+            } catch (InvocationTargetException | IllegalAccessException | InstantiationException e) {
+                System.err.println("Could not dynamically initiate doBatch");
+                throw new RuntimeException(e);
+            }
+        });
+        t.start();
+        return t;
     }
 }
