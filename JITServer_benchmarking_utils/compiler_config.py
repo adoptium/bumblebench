dcfdd5a5519124bd271db7986757fbe7553fe980
RichardUofT <richard.kha@mail.utoronto.ca>
2024-05-16 15:36:40 -0400

    Fixed compiler_config.json and compiler_config.py to be more readable

--- JITServer_benchmarking_utils/compiler_config.py
+++ JITServer_benchmarking_utils/compiler_config.py
@@ -5,26 +5,37 @@ def get_compiler_args(json_file, log_directory):
     config = json.load(open(json_file, 'r'))
 
     xjit_flags = '-Xjit:'
-    xaot_flags = '-Xaot'
+    xaot_flags = '-Xaot:'
     other_flags = ''
     for key in config.keys():
-        if key.startswith("-Xjit"):
+        if key.startswith("JIT"):
             strings = key.split(":")
-            if strings[1] == "count":
-                xjit_flags += "count" + '=' + str(config[key]) + ","
-            if strings[1] == "compiler_method_options":
+            strings[1] = strings[1].strip()
+            if strings[1] == "global_invocation_count_till_compiled":
+                xjit_flags += "count=" + str(config[key]) + ","
+            if strings[1] == "method_configs":
                 for kernel_conf in config[key]:
-                    xjit_flags += "'{" + kernel_conf["method_signature"] + "}(" + kernel_conf["options"] + ")',"
-            if strings[1] == "verbose,vlog":
+                    xjit_flags += "'{" + kernel_conf["method_signature"] + "}("
+                    if "temperature" in kernel_conf.keys():
+                        xjit_flags += "optLevel=" + kernel_conf["temperature"] + ","
+                    if "invocation_count_till_compiled" in kernel_conf.keys():
+                        xjit_flags += "count=" + str(kernel_conf["invocation_count_till_compiled"])
+                    xjit_flags += ")',"
+            if strings[1] == "log_file":
                 xjit_flags += "verbose,vlog" + '=' + log_directory + "/" + str(config[key]) + ","
-        elif key.startswith("-Xaot"):
+        elif key.startswith("AOT"):
             strings = key.split(":")
-            if strings[1] == "count":
-                xaot_flags += ":count" + '=' + str(config[key]) + ","
-        elif key == "-Xnoaot":
-            if not config[key]:
-                other_flags += key + " "
-        elif key == "-XX:UseJITServer":
-            sign = "+" if config[key] else "-"
-            other_flags += "-XX:" + sign + "UseJITServer"
+            strings[1] = strings[1].strip()
+            if strings[1] == "enable_aot":
+                if not config[key]:
+                    xaot_flags = "-Xnoaot"
+            if xaot_flags != "-Xnoaot":
+                if strings[1] == "aot_count":
+                    xaot_flags += "count" + '=' + str(config[key]) + ","
+        elif key.startswith("VM"):
+            strings = key.split(":")
+            strings[1] = strings[1].strip()
+            if strings[1] == "use_JIT_server":
+                if config[key]:
+                    other_flags += "-XX:+UseJITServer"
     return xjit_flags, xaot_flags, other_flags
