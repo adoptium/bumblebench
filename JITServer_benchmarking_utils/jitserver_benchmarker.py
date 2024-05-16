22c1ff0c3c01c4e79791e16e70cf5342abe8dec9
Nikhil <yde773786@gmail.com>
2024-05-16 14:07:27 -0400

    split configs

--- JITServer_benchmarking_utils/jitserver_benchmarker.py
+++ JITServer_benchmarking_utils/jitserver_benchmarker.py
@@ -1,5 +1,7 @@
 import os
 import json
+from compiler_config import get_compiler_args
+from kernel_config import setup_kernel_args
 import argparse
 from pathlib import Path
 import shutil
@@ -13,55 +15,29 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument('-o', '--openj9_path', required=True)
-parser.add_argument('-c', '--configuration', required=True)
+parser.add_argument('-c', '--compiler_configuration', required=True)
 parser.add_argument('-b', '--bumblebench_jitserver_path', required=True)
 parser.add_argument('-l', '--loud_output', action='store_true')
+parser.add_argument('-k', '--kernel_configuration', required=True)
 
 args = vars(parser.parse_args())
 
-json_file = args['configuration']
+compiler_json_file = args['compiler_configuration']
+kernel_json_file = args['kernel_configuration']
 openj9_path = args['openj9_path']
 bumblebench_jitserver_path = args['bumblebench_jitserver_path']
 loud_output = args['loud_output']
 
 jit_server_args = open('./JITServerArgs.txt', 'w')
-
-config = json.load(open(json_file, 'r'))
-log_directory = config_comparer.create_unique_hash_from_path(json_file, True)
-Path(log_directory).mkdir(parents=True, exist_ok=True)
-xjit_flags = '-Xjit:'
-xaot_flags = '-Xaot'
-other_flags = ''
-for key in config.keys():
-    if key.startswith("-Xjit"):
-        strings = key.split(":")
-        if strings[1] == "count":
-            xjit_flags += "count" + '=' + str(config[key]) + ","
-        if strings[1] == "compiler_method_options":
-            for kernel_conf in config[key]:
-                xjit_flags += "'{" + kernel_conf["method_signature"] + "}(" + kernel_conf["options"] + ")',"
-        if strings[1] == "verbose,vlog":
-            xjit_flags += "verbose,vlog" + '=' + log_directory + "/" + str(config[key]) + ","
-    elif key == "kernels":
-        jit_server_args.write('BumbleBench.classesToInvoc=')
-        for kernel_conf in config['kernels']:
-            jit_server_args.write(f'{kernel_conf["kernel_name"]} {kernel_conf["invoc_count"]}')
-    elif key.startswith("-Xaot"):
-        strings = key.split(":")
-        if strings[1] == "count":
-            xaot_flags += ":count" + '=' + str(config[key]) + ","
-    elif key == "-Xnoaot":
-        if not config[key]:
-            other_flags += key + " "
-    elif key == "-XX:UseJITServer":
-        sign = "+" if config[key] else "-"
-        other_flags += "-XX:" + sign + "UseJITServer"
-
-jit_server_args.close()
+log_directory, xjit_flags, xaot_flags, other_flags = get_compiler_args(compiler_json_file)
+setup_kernel_args(kernel_json_file)
 
 now = str(datetime.now())
 now = now.replace(" ", ".").replace(":", "").replace("-","")
-shutil.copy(json_file, log_directory + "/config.json")
+shutil.copy(compiler_json_file, log_directory + "/compiler_config.json")
+
+shutil.copy(kernel_json_file, log_directory + "/kernel_config.json")
+
 if loud_output:
     print(
         f'{openj9_path} -jar {xjit_flags} {xaot_flags} {other_flags} {bumblebench_jitserver_path}/BumbleBench.jar JITserver')
